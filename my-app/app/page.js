8ccc63f9a4dcd2f5e9954c3ca443365e4dca68e6
AmrenderSinghDagar <asdagar5152@gmail.com>
2025-04-18 11:28:08 +0530

    changes U to A

---
+++ my-app/app/page.js
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+<>
+<h5 className="text-red-500 text-2xl bg-black text-center rounded-full px-3 py-1">Hello Dry Fruits Bites</h5>
+</>
+  );
+}
